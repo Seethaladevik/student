0bb8b1dd2e9046793864a9a77db986548fa00ae8
Seethaladevik <deviseethala62@gmail.com>
2025-07-13 22:57:02 +0530

    new

--- DAY76/reac76/src/main.jsx
+++ DAY76/reac76/src/main.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
 import './index.css'
 import Browse from './browse'
+import { KnockProvider } from '@knocklabs/react'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <Browse />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// Fetch the user token from FastAPI backend
+fetch("http://localhost:8000/knock-token")
+  .then((res) => res.json())
+  .then((data) => {
+    const token = data.user_token;
+
+    root.render(
+      <React.StrictMode>
+        <KnockProvider
+          apiKey="pk_cBlwgRRBkMc-3KKgA0J7W-q7LbxjyDuL1u3XXU164p4"
+          userId="Jannu"
+          userToken={token}
+        >
+          <Browse />
+        </KnockProvider>
+      </React.StrictMode>
+    );
+  })
+  .catch((err) => {
+    console.error("❌ Failed to fetch Knock token:", err);
+    root.render(<div>Failed to initialize Knock</div>);
+  });
